cf7c307ed764acb7bb2b314b533f93c16a2ea145
Hobbyshop <marc.deike@outlook.de>
2023-02-21 01:17:24 +0100

    template

---
+++ src/main/java/net/fabricmc/template/mixin/MixinMinecraftClient.java
@@ -0,0 +1,18 @@
+package net.fabricmc.template.mixin;
+
+import net.fabricmc.template.ModTemplate;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MixinMinecraftClient {
+
+    @Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Recorder;startTick()V"))
+    public void run(CallbackInfo ci) {
+        ModTemplate.LOGGER.info("Tick");
+    }
+
+}
